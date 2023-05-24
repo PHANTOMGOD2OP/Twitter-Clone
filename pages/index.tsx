27986b8fe5b30a7cba1cb17bbdb4d70647adc823
PHANTOMGOD <113450739+PHANTOMGOD2OP@users.noreply.github.com>
2023-05-24 18:18:43 +0530

    Update index.tsx now

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import PostFeed from "@/components/posts/PostFeed"
 import Header from "@/components/Header"
 import Form from "@/components/Form"
 export default function Home() {
+console.log("db", process.env.DATABASE_URL);
   return (
     <>
       <Header label="Home" />
