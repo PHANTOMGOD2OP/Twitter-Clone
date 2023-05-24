ca80903c130c2c8f08b4abbaf00c907f9d883596
PHANTOMGOD <113450739+PHANTOMGOD2OP@users.noreply.github.com>
2023-05-24 18:14:44 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header"
 import Form from "@/components/Form"
 export default function Home() {
   return (
-   console.log("db", process.env.DATABASE_URL);
     <>
       <Header label="Home" />
       <Form placeholder="What's happening?" />
