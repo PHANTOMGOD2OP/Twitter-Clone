58741c34d93f9731de8c51a33e5502348b0bc288
PHANTOMGOD <113450739+PHANTOMGOD2OP@users.noreply.github.com>
2023-05-24 18:00:52 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 
-console.log("db", process.env.DATABASE_URL) in re.
+console.log("db", process.env.DATABASE_URL)
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
